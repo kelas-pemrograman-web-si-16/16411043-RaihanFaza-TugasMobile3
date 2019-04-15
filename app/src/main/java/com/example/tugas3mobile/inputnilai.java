17e19de0ca75aeccc6f8a7ad9f4b6486396786db
raihanfaza <raihanmkt25@gmail.com>
2019-04-15 09:58:17 +0700

    first commit

---
+++ app/src/main/java/com/example/tugas3mobile/inputnilai.java
@@ -0,0 +1,62 @@
+package com.example.tugas3mobile;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class inputnilai extends AppCompatActivity {
+
+    @BindView(R.id.edNpm)
+    EditText edNpm;
+    @BindView(R.id.edNama)
+    EditText edNama;
+    @BindView(R.id.edTugas)
+    EditText edTugas;
+    @BindView(R.id.edQuis)
+    EditText edQuis;
+    @BindView(R.id.edUts)
+    EditText edUts;
+    @BindView(R.id.edUas)
+    EditText edUas;
+
+    String strNpm, strNama;
+    double dblTugas, dblQuis, dblUts, dblUas, dblHasil;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inputnilai);
+        ButterKnife.bind(this);
+
+        Intent getData = getIntent();
+        strNpm = getData.getStringExtra("npm");
+        strNama= getData.getStringExtra("nama");
+
+        edNpm.setText(strNpm);
+        edNama.setText(strNama);
+    }
+
+    @OnClick(R.id.btnHitung)
+    void btnHitung(){
+        dblTugas = Double.parseDouble(edTugas.getText().toString());
+        dblQuis  = Double.parseDouble(edQuis.getText().toString());
+        dblUts   = Double.parseDouble(edUts.getText().toString());
+        dblUas   = Double.parseDouble(edUas.getText().toString());
+
+        dblHasil = (dblTugas + dblQuis + dblUts + dblUas) / 4;
+
+        //Toast.makeText(getApplicationContext(), String.valueOf(dblHasil),
+        //        Toast.LENGTH_LONG).show();
+        Intent a = new Intent(inputnilai.this, hasil.class);
+        a.putExtra("nama", strNama);
+        a.putExtra("npm", strNpm);
+        a.putExtra("nilai", String.valueOf(dblHasil));
+        startActivity(a);
+        finish();
+}
+}
